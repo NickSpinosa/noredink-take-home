d741395b997769412afb521652f24b706942b285
NickSpinosa <nickspinosa1022@gmail.com>
2018-08-04 13:16:38 -0400

    initial project setup

---
+++ src/main/java/com/nick/spinosa/noredinktakehome/NoredinkTakeHomeApplication.java
@@ -0,0 +1,12 @@
+package com.nick.spinosa.noredinktakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoredinkTakeHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoredinkTakeHomeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nick/spinosa/noredinktakehome/NoredinkTakeHomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nick.spinosa.noredinktakehome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoredinkTakeHomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
